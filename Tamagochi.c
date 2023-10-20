586e0e866ecfb837d6f08ce89089a94857827578
Dylan <dpaez@ucol.mx>
2023-10-20 17:59:24 -0600

    Tamagochi was created

---
+++ Tamagochi.c
@@ -0,0 +1,16 @@
+/* Nombre: Dylan Tristan Paez Deniz
+ * Grado y Grupo: 1°D
+ * Descripción: Desarrollar un programa básico que simule un Tamagochi
+ * mediante bucles,condicionales y operadores lógicos
+ */
+#include<stdio.h>
+int main()
+  {
+	  printf("\t Tamagochi");
+	  printf("\n Menú de acciones:\n");
+	  printf("Jugar\n");
+	  printf("Comer\n");
+	  printf("Dormir\n");
+	  printf("Ver estado\n");
+	  printf("Salir");
+  }
