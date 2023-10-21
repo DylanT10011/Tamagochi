a4c3b32679fdf9dc8bcb4856d967ad6d0a508c17
Dylan <dpaez@ucol.mx>
2023-10-21 02:25:26 -0600

    Activities has been added

--- Tamagochi.c
+++ Tamagochi.c
@@ -6,7 +6,7 @@
 #include<stdio.h>
 int main()
   {
-	  int opcion,subopc1;
+	  int opcion,subopc1,health=50,energy=100;
 	  printf("\t Tamagochi");
 	  printf("\n \t Menú de acciones:\n");
 	  printf("\t 1._Jugar\n");
@@ -14,6 +14,7 @@ int main()
 	  printf("\t 3._Dormir\n");
 	  printf(" \t 4._Ver estado\n");
 	  printf("\t 5._ Salir");
+	  printf("\n Elige una opción: ");
 	  scanf("%d",&opcion);
 	  switch (opcion)
 	    {
@@ -24,14 +25,18 @@ int main()
 			    printf("\n \t2._Jugar Con un juguete");
 			    printf("\n \t3._Jugar al escondite");
 			    printf("\n \t4._No Jugar");
+			    printf("\nSelecciona una opción: ");
 			    scanf("%d",&subopc1);
 			    switch (subopc1)
 			      {
-				      case 1: 
+				      case 1: energy = energy - 60;
+					     printf("Uff...Eso fue muy agotador y divertido a la vez"); 
 					      break;
-				      case 2: 
+				      case 2: energy = energy - 12;
+					      printf("Eso fue muy divertifo c:");
 					      break;
-				      case 3: 
+				      case 3: energy = energy - 30;
+					     printf("Me divertí mucho"); 
 					      break;
 				      case 4: 
 					      break;
