abffe3521d6031b033882cebd2eb901176bf3c66
Dylan <dpaez@ucol.mx>
2023-10-21 02:07:35 -0600

    Few visual corrections

--- Tamagochi.c
+++ Tamagochi.c
@@ -8,22 +8,23 @@ int main()
   {
 	  int opcion,subopc1;
 	  printf("\t Tamagochi");
-	  printf("\n Menú de acciones:\n");
-	  printf("Jugar\n");
-	  printf("Comer\n");
-	  printf("Dormir\n");
-	  printf("Ver estado\n");
-	  printf("Salir");
+	  printf("\n \t Menú de acciones:\n");
+	  printf("\t 1._Jugar\n");
+	  printf("\t 2._Comer\n");
+	  printf("\t 3._Dormir\n");
+	  printf(" \t 4._Ver estado\n");
+	  printf("\t 5._ Salir");
 	  scanf("%d",&opcion);
 	  switch (opcion)
 	    {
 		    case 1: 
 			    printf("\t¿Donde deberíamos jugar?");
+			 
+			    printf("\n \t1._Jugar En el parque");
+			    printf("\n \t2._Jugar Con un juguete");
+			    printf("\n \t3._Jugar al escondite");
+			    printf("\n \t4._No Jugar");
 			    scanf("%d",&subopc1);
-			    printf("\nJugar En el parque");
-			    printf("\nJugar Con un juguete");
-			    printf("\nJugar al escondite");
-			    printf("\nNo Jugar");
 			    switch (subopc1)
 			      {
 				      case 1: 
