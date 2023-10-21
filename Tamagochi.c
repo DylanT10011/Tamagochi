8fa3ccf10ebdd70ef6f3df988570aff14a10e8e6
Dylan <dpaez@ucol.mx>
2023-10-20 20:17:11 -0600

    Food menu has been added

--- Tamagochi.c
+++ Tamagochi.c
@@ -6,6 +6,7 @@
 #include<stdio.h>
 int main()
   {
+	  int opcion,subopc1,subopc2;
 	  printf("\t Tamagochi");
 	  printf("\n Menú de acciones:\n");
 	  printf("Jugar\n");
@@ -13,4 +14,19 @@ int main()
 	  printf("Dormir\n");
 	  printf("Ver estado\n");
 	  printf("Salir");
+	  scanf("%d",&opcion);
+
+	  switch(opcion)
+	    {
+		    case 1:
+			    break;
+		    case 2: 
+			    printf("\t¿Qué vamos a comer?\n");
+			    scanf("%d",&subopc2);
+			    printf("\nComer carne");
+			    printf("\nComer frutas");
+			    printf("\nComer comida rápida");
+			    printf("\nNo comer");
+			    break;
+	    }
   }
